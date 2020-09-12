c761b42efe469a9cbe7c33840f20c8202db1c8db
Fabiane Santos <fanutri1@gmail.com>
2020-09-12 19:12:37 -0300

    Updates

---
+++ precache-manifest.f90ab9e391331327aeeb8f4e3b89e7b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877543dea1963a4eff09bad91ee6648a",
+    "url": "/FabianeSantos/burger-queen/index.html"
+  },
+  {
+    "revision": "45a3dfe452b1e69ed7ee",
+    "url": "/FabianeSantos/burger-queen/static/css/main.e401ec0b.chunk.css"
+  },
+  {
+    "revision": "4abd77c00b660cb1e904",
+    "url": "/FabianeSantos/burger-queen/static/js/2.f1a17f6b.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/FabianeSantos/burger-queen/static/js/2.f1a17f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a3dfe452b1e69ed7ee",
+    "url": "/FabianeSantos/burger-queen/static/js/main.097d71f0.chunk.js"
+  },
+  {
+    "revision": "5c35fb207f80aaa4af57",
+    "url": "/FabianeSantos/burger-queen/static/js/runtime-main.98da474e.js"
+  },
+  {
+    "revision": "45b124f7c416a04841dfaea6b4bc58ff",
+    "url": "/FabianeSantos/burger-queen/static/media/campana.45b124f7.png"
+  },
+  {
+    "revision": "31671874b2763c9136558c69ff5b314e",
+    "url": "/FabianeSantos/burger-queen/static/media/logo.31671874.png"
+  }
+]);
